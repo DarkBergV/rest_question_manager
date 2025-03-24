e222b8a51d5590f555309a62f1632cb15a074f59
Gabriel Andrade <74016991+DarkBergV@users.noreply.github.com>
2025-03-24 17:24:45 -0300

    fetch data from an api and displayed on a table

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -1,12 +1,40 @@
 
-async function get_data() {
-    const response = await fetch("http://localhost:3000/home")
+document.addEventListener("DOMContentLoaded", function(event){
+    async function get_data() {
+    const response = await fetch("http://localhost:3000/home", {
+        mode:"cors"})
 
-    const json_file = await response.json();
-    console.log(json_file)
+    const json_file = await response.json()
+    return json_file
+  
+    
+
         
  
 
 }
+async function table() {
+    let table_test = document.getElementById('table')
+    let row = document.createElement('tr')
 
-get_data()
+
+    data = await get_data()
+    
+    for (i = 0; i < data.message.length; i++){
+        let row = document.createElement('tr')
+
+        for (const x in data.message[i]){
+            console.log(data.message[i][x])
+            let cell = document.createElement('th')
+            cell.append(data.message[i][x])
+            row.append(cell)    
+        }
+
+        table_test.append(row)
+
+        
+    }
+    
+}
+
+table()})
