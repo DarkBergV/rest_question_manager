1290be08928a5b98c0b782ee8e1edb77a99825b3
Gabriel Andrade <74016991+DarkBergV@users.noreply.github.com>
2025-03-27 16:01:40 -0300

    added delete path on application

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function(event){
    
     async function get_data() {
@@ -37,7 +36,8 @@ document.getElementById("form-submit").addEventListener("click", async function(
     })
     .then(response => response.json())
     .then(res => {
-        if (res.status === 'success'){
+        console.log(res)
+        if (res.status === 200){
             alert("asd")
         }
     })
@@ -45,6 +45,54 @@ document.getElementById("form-submit").addEventListener("click", async function(
     
     
 })
+
+async function delete_data(id) {
+    
+    id = JSON.stringify({"client_id": id})
+    await fetch(`http://localhost:3000/delete/:${id}`, {
+        method : 'DELETE',
+        headers:{
+            "content-type":"application/json"
+        },
+        mode:"cors",
+        body:(id)
+    })
+    .then(response =>response.json())
+    .then(res => {
+        console.log(res)
+        if (res.status===404){
+            alert('not found')
+        }else if(res.status===200){
+            alert('success')
+        }
+    })
+    
+}
+
+async function createDelete(id) {
+    // create form and buttons
+    let delete_form = document.createElement('form')
+    delete_form.method = "DELETE"
+    console.log(id)
+    let delete_space = document.createElement('th')
+    let delete_button = document.createElement('button')
+    let delete_figure = document.createTextNode('\u00D7')
+    delete_button.type = 'submit'
+
+    //add figure to button and button to form
+    delete_button.append(delete_figure)
+    delete_form.append(delete_button)
+
+    //button logic when clicked
+    delete_button.addEventListener('click', async function (e) {
+        e.preventDefault();
+        await delete_data(id,e)  
+    })
+
+    //add button to space on the table 
+    delete_space.append(delete_form)
+    return delete_space
+}
 async function table() {
     let table_test = document.getElementById('table')
     let row = document.createElement('tr')
@@ -54,14 +102,19 @@ async function table() {
     
     for (i = 0; i < data.message.length; i++){
         let row = document.createElement('tr')
-
+        let id 
         for (const x in data.message[i]){
-            
+                
             let cell = document.createElement('th')
             cell.append(data.message[i][x])
+            if (x === 'question_id'){
+                id = data.message[i][x]
+            }
             row.append(cell)    
         }
-
+        
+        row.append(await createDelete(id))
+        
         table_test.append(row)
 
         
